62b88936fe6436b94fa0000e81fec0ba86472ba9
Bryan Killian <carnoco@gmail.com>
2020-01-07 14:12:33 -0500

    convert to ts and support PR and Push

--- main.ts
+++ main.ts
@@ -1,25 +1,94 @@
 // External Dependencies
-const fs = require('fs');
-const gh = require('@actions/github');
+import * as process from 'os'
+import * as fs from 'fs'
+import * as gh from '@actions/github'
+import * as core from '@actions/core'
 
 
-const commits = gh.event.commits.filter(c => c.distinct);
+class ChangedFiles {
+    updated: Array<string> = []
+    created: Array<string> = []
+    deleted: Array<string> = []
+    files: Array<string> = []
+}
 
-const FILES = [];
-const FILES_MODIFIED = [];
-const FILES_ADDED = [];
-const FILES_DELETED = [];
+async function sortChangedFiles(files: Array<any>): Promise<ChangedFiles> {
+    return files.reduce((acc: ChangedFiles, f) => {
+        if (f.status === 'added' || f.added) {
+            acc.created.push((f.filename = undefined) ? f.added : f.filename)
+            acc.files.push((f.filename = undefined) ? f.added : f.filename)
+        }
+        if (f.status === 'removed' || f.removed) {
+            acc.deleted.push((f.filename = undefined) ? f.removed : f.filename)
+        } 
+        if (f.status === 'modified' || f.modified) {
+            acc.updated.push((f.filename = undefined) ? f.modified : f.filename)
+            acc.files.push((f.filename = undefined) ? f.modified : f.filename)
+        } 
+        if (f.status === 'renamed') {
+            acc.created.push(f.filename)
+            acc.deleted.push(f['previous_filename'])
+        }
+        return acc
+    }, new ChangedFiles())
+}
 
-commits.forEach(commit => {
-    FILES.push(...commit.modified, ...commit.added);
-    FILES_MODIFIED.push(...commit.modified);
-    FILES_ADDED.push(...commit.added);
-    FILES_DELETED.push(...commit.removed);
-});
+async function getChangedPRFiles(client: gh.GitHub, prNumber: number): Promise<ChangedFiles> {
+    const response = await client.pulls.listFiles({
+        owner: gh.context.repo.owner,
+        repo: gh.context.repo.repo,
+        pull_number: prNumber,
+    })
+    return sortChangedFiles(response.data)
+}
 
-fs.writeFileSync(`${process.env.HOME}/files.json`, JSON.stringify(FILES), 'utf-8');
-fs.writeFileSync(`${process.env.HOME}/files_modified.json`, JSON.stringify(FILES_MODIFIED), 'utf-8');
-fs.writeFileSync(`${process.env.HOME}/files_added.json`, JSON.stringify(FILES_ADDED), 'utf-8');
-fs.writeFileSync(`${process.env.HOME}/files_deleted.json`, JSON.stringify(FILES_DELETED), 'utf-8');
+async function getChangedPushFiles(commits: Array<gh.events.commits>): Promise<ChangedFiles> {
+    const distinctCommits = commits.filter(c => c.distinct)
+    return sortChangedFiles(distinctCommits)
+}
 
-process.exit(0);
+function getPrNumber(): number | null {
+    const pr = gh.context.payload.pull_request
+    return pr ? pr.number : null
+}
+
+// figure out if it is a PR or Push
+async function run(): Promise<void> {
+    try {
+        const token: string = core.getInput('repo-token')
+        const client = new gh.GitHub(token)
+        const eventName: string = gh.context.eventName
+        let changedFiles: ChangedFiles = undefined
+        if (eventName == 'push') {
+            // do push actions
+            changedFiles = await getChangedPushFiles(gh.event.commits)
+        } else if (eventName == 'pullRequest') {
+            // do PR actions
+            const prNumber = getPrNumber()
+            if (prNumber != null) {
+                changedFiles = await getChangedPRFiles(client, prNumber)
+            } else {
+                core.setFailed('Could not get pull request number from context, exiting')
+                return
+            }
+        } else {
+            core.setFailed(`Change not initiated by a PR or Push, it was ${eventName} instead.`)
+            return
+        }        
+        //write files to preserve original functionality
+        fs.writeFileSync(`${process.env.HOME}/files.json`, JSON.stringify(changedFiles.files), 'utf-8');
+        fs.writeFileSync(`${process.env.HOME}/files_modified.json`, JSON.stringify(changedFiles.updated), 'utf-8');
+        fs.writeFileSync(`${process.env.HOME}/files_added.json`, JSON.stringify(changedFiles.created), 'utf-8');
+        fs.writeFileSync(`${process.env.HOME}/files_deleted.json`, JSON.stringify(changedFiles.deleted), 'utf-8');
+        //also export some outputs
+        core.setOutput('files_created', changedFiles.created.join(' '))
+        core.setOutput('files_updated', changedFiles.updated.join(' '))
+        core.setOutput('files_deleted', changedFiles.deleted.join(' '))
+        process.exit(0);
+    } catch (error) {
+        core.error(error)
+        core.setFailed(error.message)
+    }
+}
+
+run()
